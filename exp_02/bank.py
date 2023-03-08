9caf0734b004cb6bd2df792a31d8ec1ff0db4de8
gpc <1278948168@qq.com>
2023-03-08 18:55:29 +0800

    新增任务2：找出刷卡金额排在前15的发卡银行

--- exp_02/bank.py
+++ exp_02/bank.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import csv as cs
 
 # 读入ch04_case_transRecord.xlsx文件，将其放置在名为transrecord的dataframe中
-transrecord = pd.read_excel('ch04_case_transRecord.xlsx', 'Sheet1')
+transrecord = pd.read_excel('exp_02\\ch04_case_transRecord.xlsx', 'Sheet1')
 
 # 读入ch03_case_binNum.txt文件，因为该txt文件内部是用逗号分隔的卡号-卡种数据对，
 # 因此将其当成符号分隔值文件csv读取，并将其转换成dataframe数据对象bcfile
 
-bcfile = cs.reader(open('ch04_case_binNum.txt', 'r', encoding='UTF-8'))
+bcfile = cs.reader(open('exp_02\\ch04_case_binNum.txt', 'r', encoding='UTF-8'))
 
 carddata = []
 code = []
@@ -43,7 +44,7 @@ def extractFirst6(s):
 transrecord['卡号前6位'] = transrecord['卡号'].apply(lambda s: extractFirst6(s))
 
 
-# 将前6为卡号与carddata中数据进行匹配查找，确定该笔交易的银行卡信息完成
+# 将前6位卡号与carddata中数据进行匹配查找，确定该笔交易的银行卡信息完成
 def cardinfo(s):
     t = "".join(list(code2card[code2card.code == s].cardinfo))
     return t
@@ -74,19 +75,24 @@ transrecord.dropna(inplace=True)
 
 # 按照匹配的发卡行结果对消费金额求和，本例中只有“金额”列是浮点数据，可以进行求和运算
 # 找出刷卡金额排在前10的发卡银行
-sumbank = transrecord.groupby('匹配发卡行').sum()
-top10bank = sumbank.sort_values(by='金额', ascending=False)[:10].copy()
-print(top10bank)
+sumbank = transrecord.groupby('匹配发卡行').agg({'金额': 'sum'})
+top15bank = sumbank.sort_values(by=['金额'], ascending=False)[:15].copy()
+
+print("刷卡金额排在前15的发卡银行为：")
+print(top15bank)
 
 # 绘制图表，这里使用到了matplotlib，后续章节将对其有详细介绍
-import matplotlib.pyplot as plt
 
-#get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 # notebook使用
 plt.rcParams['font.sans-serif'] = ['SimHei']
 
-fig, ax = plt.subplots(figsize=(15, 8), dpi=300)
-plt.plot(top10bank.index, top10bank['金额'])
-plt.xticks(fontsize=15)
-plt.savefig("ch04_case_bankcard.png", dpi=300, bbox_inches='tight')
-plt.show()
+fig, ax = plt.subplots(figsize=(100, 30), dpi=300)
+plt.bar(top15bank.index, top15bank['金额'])
+ax.set_title('刷卡金额排在前15的发卡银行', fontsize=50, pad=100)
+ax.set_ylabel('金额', fontsize=50, labelpad=100)
+ax.set_xlabel('发卡银行', fontsize=50, labelpad=100)
+plt.xticks(fontsize=50)
+plt.yticks(fontsize=50)
+plt.savefig("exp_02\\ch04_case_bankcard.png", dpi=300, bbox_inches='tight')
+# plt.show()
